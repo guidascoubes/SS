d665c0be54bc3f971bc56a87a2a815c1ce6520de
guidascoubes <guiben252003@gmail.com>
2024-09-17 20:41:47 +0100

    o começo

---
+++ SS_OpenCV_Base/SS_OpenCV/ImageClass.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using System.Drawing;
+using System.Management.Instrumentation;
+using System.Windows.Forms;
+using Emgu.CV.ImgHash;
+using System.Linq;
+using Emgu.CV.XFeatures2D;
+using System.Diagnostics.Eventing.Reader;
+
+namespace SS_OpenCV
+{
+    class ImageClass
+    {
+
+        /// <summary>
+        /// Image Negative using EmguCV library
+        /// Slower method
+        /// </summary>
+        /// <param name="img">Image</param>
+        public static void Negativeold(Image<Bgr, byte> img)
+        {
+            int x, y;
+
+            Bgr aux;
+            for (y = 0; y < img.Height; y++)
+            {
+                for (x = 0; x < img.Width; x++)
+                {
+                    // acesso pela biblioteca : mais lento 
+                    aux = img[y, x];
+                    img[y, x] = new Bgr(255 - aux.Blue, 255 - aux.Green, 255 - aux.Red);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Image Negative made by student
+        /// Faster method
+        /// </summary>
+        /// <param name="img">Image</param>
+        public static void Negative(Image<Bgr, byte> img)
+        {
+            unsafe
+            {// direct access to the image memory(sequencial)
+                // direcion top left -> bottom right
+
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.ImageData.ToPointer(); // Pointer to the image
+                byte blue, green, red;
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.NChannels; // number of channels - 3
+                int padding = m.WidthStep - m.NChannels * m.Width; // alinhament bytes (padding)
+                int x, y;
+
+                for (y = 0; y < height; y++)
+                {
+                    for (x = 0; x < width; x++)
+                    {
+                        blue = dataPtr[0];
+                        green = dataPtr[1];
+                        red = dataPtr[2];
+                        // convert to the negative color
+                        dataPtr[0] = (byte)(255 - blue);
+                        dataPtr[1] = (byte)(255 - green);
+                        dataPtr[2] = (byte)(255 - red);
+
+                        // advance the pointer to the next pixel
+                        dataPtr += nChan;
+                    }
+                    //at the end of the line advance the pointer by the aligment bytes (padding)
+                    dataPtr += padding;
+                }
+            }
+        }
+        /// <summary>
+        /// Convert to gray
+        /// Direct access to memory - faster method
+        /// </summary>
+        /// <param name="img">image</param>
+        public static void ConvertToGray(Image<Bgr, byte> img)
+        {
+            unsafe
+            {
+                // direct access to the image memory(sequencial)
+                // direcion top left -> bottom right
+
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.ImageData.ToPointer(); // Pointer to the image
+                byte blue, green, red, gray;
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.NChannels; // number of channels - 3
+                int padding = m.WidthStep - m.NChannels * m.Width; // alinhament bytes (padding)
+                int x, y;
+
+                if (nChan == 3) // image in RGB
+                {
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            //retrieve 3 colour components
+                            blue = dataPtr[0];
+                            green = dataPtr[1];
+                            red = dataPtr[2];
+
+                            // convert to gray
+                            gray = (byte)Math.Round(((int)blue + green + red) / 3.0);
+
+                            // store in the image
+                            dataPtr[0] = gray;
+                            dataPtr[1] = gray;
+                            dataPtr[2] = gray;
+
+                            // advance the pointer to the next pixel
+                            dataPtr += nChan;
+                        }
+
+                        //at the end of the line advance the pointer by the aligment bytes (padding)
+                        dataPtr += padding;
+                    }
+                }
+            }
+        }
+        public static void Red(Image<Bgr, byte> img)
+        {
+            unsafe
+            {
+                // direct access to the image memory(sequencial)
+                // direcion top left -> bottom right
+
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.ImageData.ToPointer(); // Pointer to the image
+                byte red;
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.NChannels; // number of channels - 3
+                int padding = m.WidthStep - m.NChannels * m.Width; // alinhament bytes (padding)
+                int x, y;
+
+                if (nChan == 3) // image in RGB
+                {
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            //retrieve 3 colour components                       
+                            red = dataPtr[2];
+
+                            // store in the image
+                            dataPtr[0] = red;
+                            dataPtr[1] = red;
+                            dataPtr[2] = red;
+
+                            // advance the pointer to the next pixel
+                            dataPtr += nChan;
+                        }
+
+                        //at the end of the line advance the pointer by the aligment bytes (padding)
+                        dataPtr += padding;
+                    }
+                }
+            }
+
+        }
+        public static void BrightContrast(Image<Bgr, byte> img, int bright, double contrast)
+        {
+            unsafe
+            {
+                // direct access to the image memory(sequencial)
+                // direcion top left -> bottom right
+                Bgr aux;
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.ImageData.ToPointer(); // Pointer to the image
+                byte blue, green, red;
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.NChannels; // number of channels - 3
+                int padding = m.WidthStep - m.NChannels * m.Width; // alinhament bytes (padding)
+                int x, y;
+                if (nChan == 3)
+                {
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            aux = img[y, x];
+                            img[y, x] = new Bgr(contrast * aux.Blue + bright, contrast * aux.Green + bright, contrast * aux.Red + bright);
+
+                            //dataPtr[0] = (byte)(contrast * blue + bright);
+                            //dataPtr[1] = (byte)(contrast * green + bright);
+                            //dataPtr[2] = (byte )(contrast * red + bright);
+
+                            // advance the pointer to the next pixel
+                            //dataPtr += nChan;
+                        }
+
+                        //at the end of the line advance the pointer by the aligment bytes (padding)
+                        //dataPtr += padding;
+                    }
+                }
+            }
+        }
+    }
+}
